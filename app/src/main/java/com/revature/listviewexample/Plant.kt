d1cf0e2aa9a70a972b2cdcd899598358687dfe78
RyanKCox <ryankcox90@gmail.com>
2022-03-16 09:28:37 -0500

    Add plant assets and classes

---
+++ app/src/main/java/com/revature/listviewexample/Plant.kt
@@ -0,0 +1,8 @@
+package com.revature.listviewexample
+
+data class Plant(
+    val id:Int,
+    val name:String,
+    val Description:String,
+    val imageRes:Int
+    )
---
+++ app/src/main/java/com/revature/listviewexample/Plants.kt
@@ -0,0 +1,4 @@
+package com.revature.listviewexample
+
+class Plants {
+}
